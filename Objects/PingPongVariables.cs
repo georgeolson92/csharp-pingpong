86d56f1c938eec89be3586562bbdb8f858d4bad2
George Olson and Joel Delight <george+joel@>
2016-07-05 09:01:03 -0700

    give the expected result when a user inputs the number 3.

--- Objects/PingPongVariables.cs
+++ Objects/PingPongVariables.cs
@@ -5,9 +5,22 @@ namespace PingPongGenerator
 {
   public class PingPongGenerator
   {
-    public List<string> PingPongMaker(int number)
+    public List<string> PingPongMaker(int countingNumber)
     {
       List<string> pingPongList = new List<string> {};
+      for (int i=1; i <= countingNumber; i ++)
+      {
+        string stringToAdd = "";
+        if (i % 3 == 0)
+        {
+          stringToAdd = "Ping";
+        }
+        else
+        {
+          stringToAdd = i.ToString();
+        }
+        pingPongList.Add(stringToAdd);
+      }
       return pingPongList;
     }
   }
--- Tests/PingPongTest.cs
+++ Tests/PingPongTest.cs
@@ -8,7 +8,15 @@ namespace PingPongGenerator
     [Fact]
     public void PingPongMaker_ForNumberDivisibleByThree_ReturnList()
     {
+      //Arrange
+      List<string> expectedResult = new List<string> {"1", "2", "Ping"};
+      PingPongGenerator testPingPongGenerator = new PingPongGenerator();
 
+      //Act
+      List<string> result = testPingPongGenerator.PingPongMaker(3);
+
+      //Assert
+      Assert.Equal(result, expectedResult);
     }
   }
 }
